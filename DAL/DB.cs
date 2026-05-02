c56c85a05cc57324e171cbf46400bdb9a0b02c42
Alvaro-Torres <116595735+Alvaro-Torres@users.noreply.github.com>
2026-05-02 19:33:43 -0400

    login

--- DAL/DB.cs
+++ DAL/DB.cs
@@ -15,22 +15,17 @@ namespace DAL
         public static DB Instance { get { return instance; } }
         #endregion
 
-        public static MediasRepository Medias { get; set; } = new MediasRepository();
-
         static public UsersRepository Users { get; set; }
             = new UsersRepository();
 
-        static public NotificationsRepository Notifications { get; set; }
-            = new NotificationsRepository();
+      //  static public NotificationsRepository Notifications { get; set; }
+      //      = new NotificationsRepository();
 
-        static public LoginsRepository Logins { get; set; }
-            = new LoginsRepository();
+       // static public LoginsRepository Logins { get; set; }
+        //    = new LoginsRepository();
 
-        static public EventsRepository Events { get; set; }
-            = new EventsRepository();
-
-        static public Repository<UnverifiedEmail> UnverifiedEmails { get; set; }
-            = new Repository<UnverifiedEmail>();
+       // static public EventsRepository Events { get; set; }
+       //     = new EventsRepository();
 
         static public Repository<RenewPasswordCommand> RenewPasswordCommands { get; set; }
             = new Repository<RenewPasswordCommand>();
