7ef5f755bc0aaac02a62c3b4cee9708c0a63bd58
Alvaro-Torres <116595735+Alvaro-Torres@users.noreply.github.com>
2026-05-04 23:05:40 -0400

    models1

---
+++ Models/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Code { get; set; } = $"{DateTime.Now.Year}{new Random().Next(100000, 999999)}";
+        public DateTime BirthDate { get; set; } = DateTime.Now ;
+        public string Email { get; set; }
+        public string Phone { get; set; }
+
+    }
+}
---
+++ Models/Teacher.cs
@@ -0,0 +1,29 @@
+﻿using DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Models
+{
+    public class Teacher
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Code { get; set; } = $"CLG-420-{new Random().Next(10000, 99999)}";
+        public DateTime BirthDate { get; set; } = DateTime.Now;
+        public string Email { get; set; }
+        public string Phone { get; set; }
+
+        const string Avatars_Folder = @"/App_Assets/Teachers/";
+        const string Default_Avatar = @"no_avatar.png";
+        [Asset(Avatars_Folder, Default_Avatar)]
+        public string Avatar { get; set; } = Avatars_Folder + Default_Avatar;
+
+        public class Teachers : DAL.RecordsDB<Teacher>
+        {
+            public Teachers(DAL.DataBase dataBase) : base(dataBase) { }
+        }
+    }
+}
